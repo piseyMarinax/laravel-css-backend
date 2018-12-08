74a5d639e16bd234eb6e01ff52049c3952d62b87
pisey <pisey@marinax.biz>
2018-12-08 23:01:28 +0700

    - Create Structure resets and flexbox

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link rel="stylesheet" href="/css/normalize.css">
+        <link rel="stylesheet" href="/css/style.css">
+
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div class="header-top">
+                    <h1>XCast</h1>
+                    <a href="#">SING IN</a>
+                </div>
+
+                <nav>
+                    <a href="#">Catalog</a>
+                    <a href="#">Series</a>
+                    <a href="#">Podcast</a>
+                    <a href="#">Dicussion</a>
+                    <a href="#">Catelog</a>
+                </nav>
+            </div>
+        </header>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/menu', function () {
+    return view('menu');
+});
